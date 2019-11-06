11bb2ab96ebca8308a9e43c248a89c4a8dd8f7d0
Caroline <caroline@curieos.com>
2019-11-06 10:38:28 -0600

    Commit changes to sync both workspaces

---
+++ grow.local/include/config.h
@@ -0,0 +1,18 @@
+/**
+ * @Author: curieos
+ * @Date:   2019-06-19T19:14:29-05:00
+ * @Email:  caroline@curieos
+ * @Filename: config.h
+ * @Last modified by:   curieos
+ * @Last modified time: 2019-06-19T19:17:29-05:00
+ */
+
+#ifndef CONFIG_h
+#define CONFIG_h
+
+#define DEBUG
+#ifdef DEBUG
+  #define BAUDRATE 115200
+#endif
+
+#endif
--- grow.local/src/main.cpp
+++ grow.local/src/main.cpp
@@ -1,5 +1,46 @@
-#include "simba.h"
+#include <ESPAsyncWebServer.h>
 
-int main () {
-  return 0;
+AsyncWebServer Server(80);
+
+void GetRoot() {}
+
+void setup() {
+
+	
+	Server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+		AsyncWebServerResponse *response =
+			request->beginResponse(200, "application/json", "{}");
+		response->addHeader("Connection", "close");
+		request->send(response);
+	});
+
+	Server.on("/scan", HTTP_GET, [](AsyncWebServerRequest *request) {
+		String json = "[";
+		int n = WiFi.scanComplete();
+		if (n == -2) {
+			WiFi.scanNetworks(true);
+		} else if (n) {
+			for (int i = 0; i < n; ++i) {
+				if (i) json += ",";
+				json += "{";
+				json += "\"rssi\":" + String(WiFi.RSSI(i));
+				json += ",\"ssid\":\"" + WiFi.SSID(i) + "\"";
+				json += ",\"bssid\":\"" + WiFi.BSSIDstr(i) + "\"";
+				json += ",\"channel\":" + String(WiFi.channel(i));
+				json += ",\"secure\":" + String(WiFi.encryptionType(i));
+				json += ",\"hidden\":" +
+						String(WiFi.isHidden(i) ? "true" : "false");
+				json += "}";
+			}
+			WiFi.scanDelete();
+			if (WiFi.scanComplete() == -2) {
+				WiFi.scanNetworks(true);
+			}
+		}
+		json += "]";
+		request->send(200, "application/json", json);
+		json = String();
+	});
 }
+
+void loop() {}
