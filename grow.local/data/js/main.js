09d26aef7956c472af7973726372816a932400a3
Caroline <caroline@curieos.com>
2019-11-22 12:36:46 -0600

    Prettify the web interface and add the start for joining networks

--- grow.local/data/js/main.js
+++ grow.local/data/js/main.js
@@ -1,11 +1,22 @@
 var app = new Vue({
 	el: "#app",
 	data: {
-		networks: null
+		password: "",
+		selectedNetwork: {},
+		networks: [],
+		loading: false
+	},
+	methods: {
+		getNetworks: function () {
+			this.loading = true;
+			axios.get("http://grow.local/scan").then(response => {
+				this.networks = response.data;
+			}).finally(() => {
+				this.loading = false;
+			});
+		});
 	},
 	mounted() {
-		axios.get("http://grow.local/scan").then(response => {
-			this.networks = response.data;
-		});
+		this.getNetworks();
 	},
 })
