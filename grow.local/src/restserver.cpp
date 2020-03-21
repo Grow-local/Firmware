8c2aa1f33583481de374a00b3ca85b0061576f78
Caroline <caroline@curieos.com>
2020-03-21 15:01:44 -0500

    Switch to using /module/<option> syntax vs /module_info

--- grow.local/src/restserver.cpp
+++ grow.local/src/restserver.cpp
@@ -18,13 +18,13 @@ RestServer::RestServer(AsyncWebServer *server) {
 	timeClient->begin();
 	timeClient->setTimeOffset(ModuleConfig::timezone_offset);
 
-	server->on("/module_info", HTTP_GET, [](AsyncWebServerRequest *request) {
+	server->on("/module/info", HTTP_GET, [](AsyncWebServerRequest *request) {
 		char json[500] = "";
 		sprintf(json, "{\"moduleName\": \"%s\", \"ipAddress\": \"%s\"}", WiFi.macAddress().c_str(), WiFi.localIP().toString().c_str());
 		request->send(200, "application/json", json);
 	});
 
-	server->on("/setup", HTTP_POST, [](AsyncWebServerRequest *request){}, NULL, [](AsyncWebServerRequest* request, uint8_t *data, size_t len, size_t index, size_t total) {
+	server->on("/module/setup", HTTP_POST, [](AsyncWebServerRequest *request){}, NULL, [](AsyncWebServerRequest* request, uint8_t *data, size_t len, size_t index, size_t total) {
 		char raw_config[len] = "";
 		strcpy(raw_config, (const char*)data);
 		ModuleConfig::SetupModule(raw_config);
