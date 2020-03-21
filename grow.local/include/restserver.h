d3729aa73810fe7b1b3dcb38d515fa993853fea3
Caroline <caroline@curieos.com>
2020-03-21 09:48:52 -0500

    Add server methods for getting module info and setup

--- grow.local/include/restserver.h
+++ grow.local/include/restserver.h
@@ -3,9 +3,18 @@
 
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+#include "config.h"
 
 class RestServer {
-
+public:
+	RestServer(AsyncWebServer *server);
+	~RestServer();
+private:
+	WiFiUDP ntpUDP;
+	NTPClient *timeClient;
 };
 
 #endif
--- grow.local/src/restserver.cpp
+++ grow.local/src/restserver.cpp
@@ -1,1 +1,23 @@
 #include "restserver.h"
+
+RestServer::RestServer(AsyncWebServer *server) {
+	timeClient = new NTPClient(ntpUDP);
+
+	timeClient->begin();
+	timeClient->setTimeOffset(ModuleConfig::timezone_offset);
+
+	server->on("/module_info", HTTP_GET, [](AsyncWebServerRequest *request) {
+		char json[500] = "";
+		sprintf(json, "{\"moduleName\": \"%s\", \"ipAddress\": \"%s\"}", WiFi.macAddress().c_str(), WiFi.localIP().toString().c_str());
+		request->send(200, "application/json", json);
+	});
+
+	server->on("/setup", HTTP_POST, [](AsyncWebServerRequest *request){}, NULL, [](AsyncWebServerRequest* request, uint8_t *data, size_t len, size_t index, size_t total) {
+		char raw_config[len] = "";
+		strcpy(raw_config, (const char*)data);
+		ModuleConfig::SetupModule(raw_config);
+		request->send(201);
+		delay(1000);
+		ESP.restart();
+	});
+}
