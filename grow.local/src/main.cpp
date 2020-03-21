410dfb0144cabb05075a0aab864d1e3a9b827849
Caroline <caroline@curieos.com>
2020-03-21 09:25:55 -0500

    Add reading from a file to gather networks

--- grow.local/src/main.cpp
+++ grow.local/src/main.cpp
@@ -1,6 +1,8 @@
+#include <ArduinoJson.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
 #include <SPIFFS.h>
+#include <vector>
 
 #include "config.h"
 #include "restserver.h"
@@ -16,21 +18,53 @@ struct Network {
 	char password[100];
 };
 
+std::vector<Network> readAP(fs::FS &fs) {
+	// fs.remove("/ap.txt");
+	File file = fs.open("/ap.txt");
+	std::vector<Network> networks;
+	if (!file) {
+#ifdef DEBUG
+		Serial.println("Failed to open file for reading");
+#endif
+		return networks;
+	}
+
+	while (file.available()) {
+		DynamicJsonDocument n(JSON_CAPACITY);
+		auto s = file.readStringUntil('\n');
+		deserializeJson(n, s);
+		struct Network network;
+		strcpy(network.ssid, n["ssid"]);
+		strcpy(network.password, n["password"]);
+		networks.push_back(network);
+	}
+
+	file.close();
+	return networks;
+}
+
 void setup() {
 #ifdef DEBUG
 	Serial.begin(BAUDRATE);
 	delay(2000);
 #endif
 
-	WiFi.mode(WIFI_AP_STA);
 	SPIFFS.begin();
 
-	scanWifi = new ScanWifiServer(Server);
-	
-	Server->begin();
+	auto networks = readAP(SPIFFS);
+	if (networks.size() == 0) {
+#ifdef DEBUG
+		Serial.println("Starting ScanWifiServer");
+#endif
+		WiFi.mode(WIFI_AP);
 
-	MDNS.begin("grow");
-	MDNS.addService("http", "tcp", 80);
+		scanWifi = new ScanWifiServer(Server);
+
+		Server->begin();
+
+		MDNS.begin("grow");
+		MDNS.addService("http", "tcp", 80);
+	}
 }
 
 void loop() {}
